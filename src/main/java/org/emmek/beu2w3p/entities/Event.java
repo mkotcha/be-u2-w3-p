143796b3f5590c070f041a8817a594b9fee8f1a9
Matteo Falasca <matteo.falasca@emmek.org>
2023-11-17 09:47:26 +0100

    basic entities

---
+++ src/main/java/org/emmek/beu2w3p/entities/Event.java
@@ -0,0 +1,34 @@
+package org.emmek.beu2w3p.entities;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table(name = "events")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String title;
+    private String description;
+    private LocalDate date;
+    private String location;
+    @Column(name = "max_participants")
+    private int maxParticipants;
+    @ManyToMany
+    @JoinTable(name = "events_users",
+            joinColumns = @JoinColumn(name = "event_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id"))
+    @ToString.Exclude
+    private List<User> users;
+}
---
+++ src/main/java/org/emmek/beu2w3p/entities/User.java
@@ -0,0 +1,32 @@
+package org.emmek.beu2w3p.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties({"password", "authorities", "enabled", "credentialsNonExpired", "accountNonExpired", "accountNonLocked"})
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private long id;
+    private String email;
+    private String password;
+    private String name;
+    private String surname;
+    @ManyToMany
+    @JoinTable(name = "users_events",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "event_id"))
+    @ToString.Exclude
+    private List<Event> events;
+}
