cbc63e9241f91aab32df94b1fea92a6b9c489e0a
Matteo Falasca <matteo.falasca@emmek.org>
2023-11-17 14:50:52 +0100

    event control

--- src/main/java/org/emmek/beu2w3p/security/JWTTools.java
+++ src/main/java/org/emmek/beu2w3p/security/JWTTools.java
@@ -15,13 +15,12 @@ public class JWTTools {
     private String secret;
 
     public String createToken(User user) {
-
         return Jwts.builder().setSubject(String.valueOf(user.getId()))// Subject <-- A chi appartiene il token
                 .setIssuedAt(new Date(System.currentTimeMillis())) // Data di emissione (IAT - Issued At)
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24 * 7)) // Data di scadenza (Expiration Date)
                 .signWith(Keys.hmacShaKeyFor(secret.getBytes())).compact();
 
-    } // Si utilizza al login
+    }
 
     public void verifyToken(String token) {
         try {
@@ -30,12 +29,10 @@ public class JWTTools {
         } catch (Exception ex) {
             throw new UnauthorizedException("Token not valid! Please login again");
         }
-    } // Si utilizza in tutte le request autenticate
+    }
 
     public String extractIdFromToken(String token) {
         return Jwts.parserBuilder().setSigningKey(Keys.hmacShaKeyFor(secret.getBytes()))
                 .build().parseClaimsJws(token).getBody().getSubject();
-        // Nel body (payload) del token ci sono il subject( che è l'ID dell'utente), la data di emissione, e la data di scadenza.
-        // A noi interessa l'id dell'utente
     }
 }
